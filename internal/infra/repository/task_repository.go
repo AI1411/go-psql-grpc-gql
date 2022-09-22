53f7559920af6c98aaedaad05fa30aab58e769a6
AI1411 <goethe0719@gmail.com>
2022-09-22 09:01:53 +0900

    fix task repo

--- internal/infra/repository/task_repository.go
+++ internal/infra/repository/task_repository.go
@@ -1,7 +1,9 @@
 package repository
 
-struct TaskRepository struct {
-dbClient *db.Client
+import "github.com/AI1411/go-psql_grpc_gql/db"
+
+type TaskRepository struct {
+	dbClient *db.Client
 }
 
 func NewTaskRepository(dbClient *db.Client) *TaskRepository {
@@ -10,10 +12,5 @@ func NewTaskRepository(dbClient *db.Client) *TaskRepository {
 	}
 }
 
-func (r *TaskRepository) CreateTask(
-	ctx context.Context, in *grpc.CreateTaskRequest,
-) (*grpc.CreateTaskResponse, error) {
-	var tasks []grpc.Task
-	baseQuery := r.dbClient.Conn(ctx)
-	baseQuery = addWhereEq(baseQuery, "user_id", in.UserId)
+func (r *TaskRepository) ListTasks() {
 }
